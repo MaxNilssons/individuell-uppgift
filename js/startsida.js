0a2e4c8cd7efa8acbe5e1e6b6d7d25d5a89e46e5
Max Nilsson <Maxstjernbergnilsson@gmail.com>
2025-04-14 08:25:14 +0200

    corr

--- js/startsida.js
+++ js/startsida.js
@@ -2,15 +2,14 @@
 
 addMdToPage(`# Depression i Indien
 
-Välkommen till en interaktiv rapport om psykisk ohälsa bland studenter i Indien.
+En interaktiv rapport om psykisk ohälsa bland studenter i Indien.
 
 Navigera mellan sidorna i menyn för att se:
 
-- Samband mellan sömn, kost, kön och depression  
-- Skillnader mellan kön  
+- Samband mellan sömn, kost och depression  
+- Differanser mellan kön  
 - Visualiseringar med filtrering  
-- Statistiska analyser som T-test och normalfördelning (PÅ SIKT)
+- Statistiska analyser som T-test och normalfördelning (PÅ SIKT?)
 
-Rapporten bygger på ett dataset insamlat från studenter, och undersöker viktiga faktorer bakom psykisk ohälsa.
 `);
 
